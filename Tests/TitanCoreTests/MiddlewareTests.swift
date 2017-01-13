177c04c0b47bc3c5976cef281bf25f3cde8dbc70
Sebastian Kreutzberger <s.kreutzberger@googlemail.com>
2017-01-13 15:52:03 +0100

    BROKEN: evidence that callback is not tested
    How to test it?

--- Tests/TitanCoreTests/MiddlewareTests.swift
+++ Tests/TitanCoreTests/MiddlewareTests.swift
@@ -14,22 +14,34 @@ final class MiddlewareTests: XCTestCase {
         let titan = Titan()
         let expectedPath = "foo"
         let expectedBody = "bar"
-
+        
         // set some req and res properties
+        let expMutation = expectation(description: "mutates req & res")
+
         titan.middleware({ (req: inout RequestType, res: inout ResponseType) -> (RequestType, ResponseType) in
             XCTAssertEqual(req.path, "")
             XCTAssertEqual(res.body, "")
             req.path = expectedPath
             res.body = expectedBody
+            // should fail as evidence that XCTest 
+            // makes it actually in here
+            XCTAssertEqual(1, 2)
+            
+            expMutation.fulfill() // does not seem to work
             return (req, res)
         })
+        waitForExpectations(timeout: 5, handler: nil)
 
         // are res and req properties still set?
+        let expStillSet = expectation(description: "req & res are still set")
+
         titan.middleware({ (req: inout RequestType, res: inout ResponseType) -> (RequestType, ResponseType) in
             XCTAssertEqual(req.path, expectedPath)
             XCTAssertEqual(res.body, expectedBody)
+            expStillSet.fulfill()
             return (req, res)
         })
+        waitForExpectations(timeout: 5, handler: nil)
     }
 
     func testMiddlewareIsCalled() {
