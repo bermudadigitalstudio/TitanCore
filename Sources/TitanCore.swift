eae11bb4dcf5b9e6527cb995506764a7001d8018
Thomas Catterall <swizzlr@users.noreply.github.com>
2017-01-13 15:50:12 +0000

    Revert "linted source code"

--- Sources/TitanCore.swift
+++ Sources/TitanCore.swift
@@ -2,43 +2,44 @@
 public typealias Header = (String, String)
 
 public protocol RequestType {
-    var body: String { get }
-    var path: String { get }
-    var method: String { get }
-    var headers: [Header] { get }
+  var body: String { get }
+  var path: String { get }
+  var method: String { get }
+  var headers: [Header] { get }
 }
 
 public protocol ResponseType {
-    var body: String { get }
-    /// Status code. We have deliberately eschewed a status line since HTTP/2 ignores it, rarely used.
-    var code: Int { get }
-    var headers: [Header] { get }
+  var body: String { get }
+  /// Status code. We have deliberately eschewed a status line since HTTP/2 ignores it, rarely used.
+  var code: Int { get }
+  var headers: [Header] { get }
 }
 
 public struct Request {
-    public let method: String
-    public let path: String
-    public let body: String
-    public let headers: [Header]
+  public let method: String
+  public let path: String
+  public let body: String
+  public let headers: [Header]
 
-    public init(_ method: String, _ path: String, _ body: String = "", headers: [Header] = []) {
-        self.method = method
-        self.path = path
-        self.body = body
-        self.headers = headers
-    }
+  public init(_ method: String, _ path: String, _ body: String = "", headers: [Header] = []) {
+    self.method = method
+    self.path = path
+    self.body = body
+    self.headers = headers
+  }
 }
 
-public struct Response {
-    public let code: Int
-    public let body: String
-    public let headers: [Header]
 
-    public init(_ code: Int, _ body: String, headers: [Header] = []) {
-        self.code = code
-        self.body = body
-        self.headers = headers
-    }
+public struct Response {
+  public let code: Int
+  public let body: String
+  public let headers: [Header]
+
+  public init(_ code: Int, _ body: String, headers: [Header] = []) {
+    self.code = code
+    self.body = body
+    self.headers = headers
+  }
 }
 
 extension Response: ResponseType {}
@@ -47,20 +48,21 @@ extension Request: RequestType {}
 
 public typealias Middleware = (RequestType, ResponseType) -> (RequestType, ResponseType)
 public final class Titan {
-    public init() {}
-    private var middlewareStack = Array<Middleware>()
-    public func middleware(middleware: @escaping Middleware) {
-        middlewareStack.append(middleware)
-    }
-    public func app(request: RequestType) -> ResponseType {
-        typealias Result = (RequestType, ResponseType)
+  public init() {}
+  private var middlewareStack = Array<Middleware>()
+  public func middleware(middleware: @escaping Middleware) {
+    middlewareStack.append(middleware)
+  }
+  public func app(request: RequestType) -> ResponseType {
+    typealias Result = (RequestType, ResponseType)
 
-        let initialReq = request
-        let initialRes = Response(-1, "")
-        let initial: Result = (initialReq, initialRes)
-        let res = middlewareStack.reduce(initial) { (res, next) -> Result in
-            return next(res.0, res.1)
-        }
-        return res.1
+    let initialReq = request
+    let initialRes = Response(-1, "")
+    let initial: Result = (initialReq, initialRes)
+    let res = middlewareStack.reduce(initial) { (res, next) -> Result in
+      return next(res.0, res.1)
     }
+    return res.1
+  }
 }
+
