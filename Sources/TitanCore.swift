45a1aa6ac0de915aafb9c846b0b15f8da51110f5
Sebastian Kreutzberger <s.kreutzberger@googlemail.com>
2017-01-13 14:34:15 +0100

    Linted code
    via https://github.com/nicklockwood/SwiftFormat and the CLI command `swiftformat --disable unusedArguments .`

--- Sources/TitanCore.swift
+++ Sources/TitanCore.swift
@@ -2,43 +2,43 @@
 public typealias Header = (String, String)
 
 public protocol RequestType {
-  var body: String { get }
-  var path: String { get }
-  var method: String { get }
-  var headers: [Header] { get }
+    var body: String { get }
+    var path: String { get }
+    var method: String { get }
+    var headers: [Header] { get }
 }
 
 public protocol ResponseType {
-  var body: String { get }
-  /// Status code. We have deliberately eschewed a status line since HTTP/2 ignores it, rarely used.
-  var code: Int { get }
-  var headers: [Header] { get }
+    var body: String { get }
+    /// Status code. We have deliberately eschewed a status line since HTTP/2 ignores it, rarely used.
+    var code: Int { get }
+    var headers: [Header] { get }
 }
 
 public struct Request {
-  public let method: String
-  public let path: String
-  public let body: String
-  public let headers: [Header]
+    public let method: String
+    public let path: String
+    public let body: String
+    public let headers: [Header]
 
-  public init(_ method: String, _ path: String, _ body: String = "", headers: [Header] = []) {
-    self.method = method
-    self.path = path
-    self.body = body
-    self.headers = headers
-  }
+    public init(_ method: String, _ path: String, _ body: String = "", headers: [Header] = []) {
+        self.method = method
+        self.path = path
+        self.body = body
+        self.headers = headers
+    }
 }
 
 public struct Response {
-  public let code: Int
-  public let body: String
-  public let headers: [Header]
+    public let code: Int
+    public let body: String
+    public let headers: [Header]
 
-  public init(_ code: Int, _ body: String, headers: [Header] = []) {
-    self.code = code
-    self.body = body
-    self.headers = headers
-  }
+    public init(_ code: Int, _ body: String, headers: [Header] = []) {
+        self.code = code
+        self.body = body
+        self.headers = headers
+    }
 }
 
 extension Response: ResponseType {}
@@ -47,25 +47,24 @@ extension Request: RequestType {}
 
 public typealias Function = (RequestType, ResponseType) -> (RequestType, ResponseType)
 public final class Titan {
-  public init() {}
-  private var functionStack = Array<Function>()
-    
+    public init() {}
+    private var functionStack = Array<Function>()
+
     /// add a function to Titan’s request, response processing flow
-  public func addFunction(function: @escaping Function) {
-    functionStack.append(function)
-  }
-    
-    /// Titan app instance, should be given to a server
-  public func app(request: RequestType) -> ResponseType {
-    typealias Result = (RequestType, ResponseType)
-
-    let initialReq = request
-    let initialRes = Response(-1, "")
-    let initial: Result = (initialReq, initialRes)
-    let res = functionStack.reduce(initial) { (res, next) -> Result in
-      return next(res.0, res.1)
+    public func addFunction(function: @escaping Function) {
+        functionStack.append(function)
     }
-    return res.1
-  }
-}
 
+    /// Titan app instance, should be given to a server
+    public func app(request: RequestType) -> ResponseType {
+        typealias Result = (RequestType, ResponseType)
+
+        let initialReq = request
+        let initialRes = Response(-1, "")
+        let initial: Result = (initialReq, initialRes)
+        let res = functionStack.reduce(initial) { (res, next) -> Result in
+            return next(res.0, res.1)
+        }
+        return res.1
+    }
+}
--- Tests/TitanCoreTests/FunctionTests.swift
+++ Tests/TitanCoreTests/FunctionTests.swift
@@ -2,23 +2,22 @@ import XCTest
 @testable import TitanCore
 
 final class FunctionTests: XCTestCase {
-    
-  func testCanAddFunction() {
-    Titan().addFunction(function: { (req: RequestType, res: ResponseType) -> (RequestType, ResponseType) in
-      print(req)
-      print(res)
-      return (Request("GET", ""), Response(-1, ""))
-    })
-  }
 
-  func testFunctionIsCalled() {
+    func testCanAddFunction() {
+        Titan().addFunction(function: { (req: RequestType, res: ResponseType) -> (RequestType, ResponseType) in
+            print(req)
+            print(res)
+            return (Request("GET", ""), Response(-1, ""))
+        })
+    }
 
-  }
+    func testFunctionIsCalled() {
+    }
 
-  static var allTests : [(String, (FunctionTests) -> () throws -> Void)] {
-    return [
-      ("testFunctionIsCalled", testFunctionIsCalled),
-      ("testCanAddFunction", testCanAddFunction)
-    ]
-  }
+    static var allTests: [(String, (FunctionTests) -> () throws -> Void)] {
+        return [
+            ("testFunctionIsCalled", testFunctionIsCalled),
+            ("testCanAddFunction", testCanAddFunction),
+        ]
+    }
 }
